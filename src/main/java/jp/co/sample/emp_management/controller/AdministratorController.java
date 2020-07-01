2379bb1d46920dbc3d1a70e73f8881d5ae2a04dc
tairamatsuta <taira.matsuta@rakus.co.jp>
2020-07-01 13:51:29 +0900

    (2-1)ログイン後にログイン者名が表示されるよう修正

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -124,6 +124,8 @@ public class AdministratorController {
 			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
 			return toLogin();
 		}
+		System.out.println(administrator.getName());
+		session.setAttribute("administratorName", administrator.getName());
 		return "forward:/employee/showList";
 	}
 	
